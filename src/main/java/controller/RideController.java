fc1dc9a1b52fff98810ab2076e07fffffd7d2867
hthien <leviethoangthien2005@gmail.com>
2026-04-08 08:43:20 +0700

    add RideController.java

---
+++ src/main/java/controller/RideController.java
@@ -0,0 +1,43 @@
+package controller;
+
+import entity.Ride;
+import java.util.List;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import routing.Region;
+import service.RideService;
+
+@RestController
+@RequestMapping("/api/rides")
+public class RideController {
+
+    private final RideService rideService;
+
+    public RideController(RideService rideService) {
+        this.rideService = rideService;
+    }
+
+    @PostMapping("/book")
+    public ResponseEntity<Ride> bookRide(
+            @RequestBody Ride ride,
+            @RequestParam(name = "isReadOnly", defaultValue = "false") boolean isReadOnly) {
+        Ride createdRide = rideService.bookRide(ride, isReadOnly);
+        return ResponseEntity.status(HttpStatus.CREATED).body(createdRide);
+    }
+
+    @GetMapping("/history/{userId}")
+    public ResponseEntity<List<Ride>> getHistory(
+            @PathVariable Long userId,
+            @RequestParam Region region,
+            @RequestParam(name = "isReadOnly", defaultValue = "true") boolean isReadOnly) {
+        List<Ride> rideHistory = rideService.getHistory(userId, region, isReadOnly);
+        return ResponseEntity.ok(rideHistory);
+    }
+}
